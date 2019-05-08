150e999fefb255932925acd7bec5e5e5d25d5fd0
StarTrekRules <roblxisawesome@gmail.com>
2019-05-08 08:22:08 -0400

    Final repo initialiazation

--- Main.cs
+++ Main.cs
@@ -7,25 +7,7 @@ class MainClass {
         HttpServer serv = new HttpServer();
 
         serv.Get("/", (string data) => {
-            StreamReader reader = new StreamReader("./file.html");
-            string file = reader.ReadToEnd();
-            reader.Close();
-            return file;
-        });
-
-        serv.Post("/save/", (string data) => {
-            StreamReader reader = new StreamReader("./db.json");
-            string content = reader.ReadToEnd();
-            reader.Close();
-            File.Delete("./db.json");
-            StreamWriter writer = new StreamWriter("./db.json");
-            content = content.Trim().Trim(']');
-            content += content[content.Length - 1] != '[' ? ", " + data : data;
-            content += ']';
-            writer.Write(content);
-            writer.Close();
-
-            return "";
+            return "Hello World!";
         });
 
         while (true) {
